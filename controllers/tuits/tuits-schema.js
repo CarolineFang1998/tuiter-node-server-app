b26ba0a6a1fd164254cf13927d1a47018b6384f2
CarolineFang1998 <f767475251@gmail.com>
2023-04-03 21:38:27 -0700

    add create new tuit info

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -4,6 +4,10 @@ const schema = mongoose.Schema({
                                    tuit: String,
                                    likes: Number,
                                    liked: Boolean,
+                                   dislikes: Number,
+                                   disliked: Boolean,
+                                   replies: Number,
+                                   retuits: Number,
                                }, 
                                {collection: 'tuits'});
 export default schema;
